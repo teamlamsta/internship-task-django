edcc71a91bc33da6e46a684d3ba6602b862c41ef
Neeraj Babu <neerajbabu27@gmail.com>
2023-08-27 22:03:27 +0530

    Add Basic Url shortner
    Basic app with API to create short link, redirect and analytics

--- home/models.py
+++ home/models.py
@@ -1,2 +1,10 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+import requests
+
+class UrlShortner(models.Model):
+    incoming_url = models.SlugField(unique=True,max_length=200,blank=False)
+    outgoing_url = models.URLField(max_length=300,blank=False)
+    click_through_counter = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
--- home/serializers.py
+++ home/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+
+from home.models import UrlShortner
+
+class UrlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UrlShortner
+        fields = ['incoming_url', 'outgoing_url', 'click_through_counter', 'is_active']
